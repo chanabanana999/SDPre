3a07d39bec0c43ab141200f5385cd7e8593a074f
Chana Cohn <chana.cohn@gmail.com>
2019-04-27 14:41:41 -0600

    adding remaining labs for Objective 1

---
+++ Labs/Objective1Lab2.java
@@ -0,0 +1,7 @@
+public class Objective1Lab2{
+  public static void main ( String[] args) {
+    System.out.println();
+    System.out.println( "Chana Cohn \nI'm not telling you my age!" );
+    System.out.println();
+  }
+}
---
+++ Labs/Objective1Lab3.java
@@ -0,0 +1,10 @@
+public class Objective1Lab3 {
+  public static void main ( String [] args ) {
+    System.out.println();
+    System.out.println( "Name : Chana Cohn" );
+    System.out.println( "Age : Really not going to give you this one" );
+    System.out.println( "State : California" );
+    System.out.println();
+  }
+
+}
