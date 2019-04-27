b2578c96469405a46fccfe92a4becdf586861af1
Chana Cohn <chana.cohn@gmail.com>
2019-04-27 13:25:06 -0600

    Exercise 2

---
+++ JavaTheHardWay/GasolineReceipt.java
@@ -0,0 +1,15 @@
+public class GasolineReceipt {
+  public static void main( String[] args ) {
+    System.out.println( "+------------------------+" );
+    System.out.println( "|                        |" );
+    System.out.println( "|      CORNER STORE      |" );
+    System.out.println( "| 2019-04-27    12:16PM  |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Gallons:       15.123  |" );
+    System.out.println( "| Price/gallon: $ 2.75   |" );
+    System.out.println( "|                        |" );
+    System.out.println( "| Fuel Total:   $41.59   |" );
+    System.out.println( "|                        |" );
+    System.out.println( "+------------------------+" );
+  }
+}
