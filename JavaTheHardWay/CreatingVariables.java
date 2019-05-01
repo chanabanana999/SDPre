b5c30cd152743c49a99b7706e46daf3b64355193
Chana Cohn <chana.cohn@gmail.com>
2019-05-01 17:56:56 -0600

    Exercise 5 with study drill

--- JavaTheHardWay/CreatingVariables.java
+++ JavaTheHardWay/CreatingVariables.java
@@ -1,20 +1,24 @@
 public class CreatingVariables {
   public static void main(String[] args) {
-    int x, y, age;
-    double seconds, e, checking;
-    String firstName, lastName, title;
+    int x, y, age, extraint;
+    double seconds, e, checking, extradouble;
+    String firstName, lastName, title, extrastring1, extrastring2;
 
     x = 10;
     y = 400;
     age = 39;
+    extraint = 42;
 
     seconds = 4.71;
     e = 2.71828182845904523536;
     checking = 1.89;
+    extradouble = 3.14159;
 
     firstName = "Chana";
     lastName = "Cohn";
     title = "Ms.";
+    extrastring1 = "Nahara";
+    extrastring2 = "Wasser";
 
     System.out.println();
     System.out.println( "The variable x contains " + x );
@@ -23,6 +27,10 @@ public class CreatingVariables {
     System.out.println( "A favorite irrational # is Euler's number: " + e );
     System.out.println( "Hopefully you have more than $" + checking + "!" );
     System.out.println( "My name's " + title + " " + firstName + lastName );
+    System.out.println();
+    System.out.println( "Additional int variable: " + extraint );
+    System.out.println( "Additional double variable: " + extradouble );
+    System.out.println( "Additional strings: " + extrastring1 + " " + extrastring2 );
     System.out.println();
   }
 }
