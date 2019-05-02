45b4716a862909c180c19adf318c60a7b476f091
Chana Cohn <chana.cohn@gmail.com>
2019-05-01 20:19:54 -0600

    Exercise 14 with study drill

--- JavaTheHardWay/ShallowGrandmother.java
+++ JavaTheHardWay/ShallowGrandmother.java
@@ -4,8 +4,8 @@ public class ShallowGrandmother{
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
     int age;
-    double income, cute;
-    boolean allowed;
+    double income, cute, happy;
+    boolean allowed, allowed2;
 
     System.out.println();
     System.out.print( "Enter your age: " ) ;
@@ -16,11 +16,18 @@ public class ShallowGrandmother{
 
     System.out.print( "How cute are you on a scale from 0.0 to 10.0? " );
     cute = keyboard.nextDouble();
-
     allowed = ( age > 25 && age < 40 && ( income > 50000 || cute > 8.5 ) );
 
     System.out.println( "Allowed to date my grandchild? " + allowed );
     System.out.println();
-    
+
+    /* Drill in next block */
+    System.out.println( "OK, we'll try this with a happiness factor, as well.");
+    System.out.println( "How happy do you make them from 1 to 10? " );
+    happy = keyboard.nextDouble();
+    allowed2 = ( age > 25 && age < 40 && ( happy > 7 ) );
+    System.out.println( "Allowed to date my grandchild? " + allowed2 );
+    System.out.println();
+
   }
 }
