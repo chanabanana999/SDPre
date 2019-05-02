734819b42873484da8e9191f688638550b0f6abe
Chana Cohn <chana.cohn@gmail.com>
2019-05-01 21:02:37 -0600

    Exercise 19 with study drills

--- JavaTheHardWay/BMICategories.java
+++ JavaTheHardWay/BMICategories.java
@@ -3,12 +3,21 @@ import java.util.Scanner;
 public class BMICategories {
   public static void main(String[] args) {
     Scanner keyboard = new Scanner(System.in);
-    double bmi;
+    double bmi, height, weight;
 
     System.out.println();
-    System.out.print( "Enter your BMI: " );
-    bmi = keyboard.nextDouble();
+    // Code below commented out for Drill #2
+    // System.out.print( "Enter your BMI: " );
+    // bmi = keyboard.nextDouble();
 
+    System.out.print( "Enter your height in meters: " );
+    height = keyboard.nextDouble();
+
+    System.out.print( "Enter your weight in kilograms: " );
+    weight = keyboard.nextDouble();
+    bmi = weight / ( height * height);
+
+    System.out.println( "Your BMI is: " + bmi );
     System.out.print( "BMI category: " );
     if ( bmi < 15.0 ) {
       System.out.println( "very severely underweight" );
@@ -19,7 +28,10 @@ public class BMICategories {
     else if ( bmi < 18.5 ) {
       System.out.println( "underweight" );
     }
-    else if ( bmi < 25.0) {
+    // Drill 1 below
+    // if ( bmi < 25.0 ) {
+    // This makes it seem like a whole new if chain is starting.
+    else if ( bmi < 25.0 ) {
       System.out.println( "normal weight" );
     }
     else if ( bmi <  30.0 ) {
@@ -34,7 +46,6 @@ public class BMICategories {
     else {
       System.out.println( "very severely/\"morbidly\" obese" );
     }
-    
     System.out.println();
   }
 }
