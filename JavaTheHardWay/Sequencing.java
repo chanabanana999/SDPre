d31bb2b691e224281bf01fffad6699164081ce0e
Chana Cohn <chana.cohn@gmail.com>
2019-05-01 19:43:22 -0600

    Exercise 10 with study drill

--- JavaTheHardWay/Sequencing.java
+++ JavaTheHardWay/Sequencing.java
@@ -19,12 +19,13 @@ public class Sequencing {
     System.out.println( "Sales tax:\t" + salesTax );
     System.out.println( "Total cost:\t" + total);
     System.out.println();
+    
     /* Answers to Drill questions:
     The program does not compile with the 0 removed from price as is. It's
     probably undefined, then it gets used in a formula. If it's 0, it works, but
     then the rest gets messes up because multiplying by zero yields zero as
     a result.
-    
+
     if the salesTax and total are calculated in the original position, there
     is an error unless price is initialized to zero. Once the statements are
     places in the correct order, it does not matter if the price is initialized
