ba0e37fdc2ad3511ba5c72df0da247b1af4021b0
Chana Cohn <chana.cohn@gmail.com>
2019-05-01 20:11:37 -0600

    Exercise 13 with study drill

--- JavaTheHardWay/WeaselOrNot.java
+++ JavaTheHardWay/WeaselOrNot.java
@@ -9,11 +9,14 @@ public class WeaselOrNot {
     System.out.println();
     System.out.println( "Type the word \"weasel\", please." );
     word = keyboard.next();
+//    yep = "weasel".equals(word);
     yep = word.equals("weasel");
     nope = ! word.equals("weasel");
 
     System.out.println( "You typed what was requested: " + yep );
     System.out.println( "You ignored polite instructions: " + nope );
     System.out.println();
+
+    // Drill worked with weasel in front of .equals. That actually surprised me.
   }
 }
