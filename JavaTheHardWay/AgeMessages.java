54b40dd3f54656a984b02b3be895e52e0d149de9
Chana Cohn <chana.cohn@gmail.com>
2019-05-01 20:33:23 -0600

    Exercise 15 with study drill

--- JavaTheHardWay/AgeMessages.java
+++ JavaTheHardWay/AgeMessages.java
@@ -13,19 +13,49 @@ public class AgeMessages {
     if ( age < 13 ) {
       System.out.println( "\ttoo young to create a Facebook account");
     }
+    if ( age >= 13 ) {
+      System.out.println( "\told enough to create a Facebook account");
+    }
     if ( age < 16 ) {
       System.out.println( "\ttoo young to get a driver's license" );
+    }
+    if ( age >= 16 ) {
+      System.out.println( "\told enough to get a driver's license" );
     }
     if ( age < 18 ) {
       System.out.println( "\ttoo young to get a tatoo" );
     }
+    if ( age >= 18 ) {
+      System.out.println( "\told enough to get a tatoo" );
+    }
     if ( age < 21 ) {
       System.out.println( "\ttoo young to drink alcohol" );
     }
-    if (age < 35) {
+    if ( age >= 21 ) {
+      System.out.println( "\told enough to drink alcohol" );
+    }
+    if ( age < 35 ) {
       System.out.println( "\ttoo young to run for President of the U.S." );
       System.out.println( "\t\t(How sad!)" );
     }
+    if ( age >= 35 ) {
+      System.out.println( "\told enough to run for President of the U.S." );
+      }
+    if ( age < 65 ) {
+        System.out.println( "\tYou are not old enough to retire." );
+      }
+    // Drill #2 below
+    if ( age >= 65 ) {
+      System.out.println( "\tYou are old enough to retire!" );
+    }
     System.out.println();
+
+    /*
+    Drill #1
+    If you type in something over 35, it just says "You are:". We don't have a
+    condition defined for that, so nothing happens. It just ends
+
+    Drill #2 and #3 embedded above
+    */
   }
 }
