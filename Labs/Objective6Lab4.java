3f59386abb3adc2481e4e06c0a1abdc134402b72
Chana Cohn <chana.cohn@gmail.com>
2019-05-03 22:04:32 -0600

    Objective 6 Lab 4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,14 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int count = 1, sum = 0;
+
+    System.out.println();
+
+    while ( count <= 20 ) {
+      sum = sum + count;
+      count++;
+    }
+    System.out.println( sum );
+    System.out.println();
+  }
+}
