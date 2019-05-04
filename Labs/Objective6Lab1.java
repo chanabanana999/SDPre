634c33ddad99766c5b08ff676ef9f982cafdcf1f
Chana Cohn <chana.cohn@gmail.com>
2019-05-03 21:37:38 -0600

    Objective 6 Lab 1

---
+++ Labs/Objective6Lab1.java
@@ -0,0 +1,13 @@
+public class Objective6Lab1 {
+  public static void main(String[] args) {
+    int counter = 10;
+
+    System.out.println();
+
+    while ( counter > 0 ) {
+      System.out.println( counter);
+      counter = counter - 1;
+    }
+    System.out.println();
+  }
+}
