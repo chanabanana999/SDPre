ab3fada7c8bd8b70d3734bb4ea54423bb272b4d2
Chana Cohn <chana.cohn@gmail.com>
2019-05-04 13:20:26 -0600

    Objective 7 Lab 1

---
+++ Labs/Objective7Lab1.java
@@ -0,0 +1,11 @@
+public class Objective7Lab1 {
+  public static void main(String[] args) {
+
+    System.out.println();
+    printName();
+    System.out.println();
+  }
+  public static void printName() {
+    System.out.println("Hello Doug.");
+  }
+}
