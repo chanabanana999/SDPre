3ef7ab6c737d0bf00de5354793118d3fbcde59e1
Chana Cohn <chana.cohn@gmail.com>
2019-05-03 21:44:00 -0600

    Objective 6 Lab 2

---
+++ Labs/Objective6Lab2.java
@@ -0,0 +1,16 @@
+public class Objective6Lab2 {
+  public static void main(String[] args) {
+    int counter = 0;
+
+    System.out.println();
+
+    // Note that the instructions say to print values 0 - 20, but the
+    // example shows 1 - 20. The block below follows the instructions, not
+    // the example/
+    while ( counter <= 20 ) {
+      System.out.println( counter);
+      counter = counter + 1;
+    }
+    System.out.println();
+  }
+}
