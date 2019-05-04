381182f284d74d8c733c1d0fe35a044a13cbc06b
Chana Cohn <chana.cohn@gmail.com>
2019-05-04 13:23:53 -0600

    Objective 7 Lab 2

---
+++ Labs/Objective7Lab2.java
@@ -0,0 +1,13 @@
+public class Objective7Lab2 {
+  public static void main(String[] args) {
+    System.out.println();
+    System.out.println("In main");
+    printMessage();
+    System.out.println("In main");
+    System.out.println();
+  }
+
+  public static void printMessage() {
+    System.out.println("In method");
+  }
+}
